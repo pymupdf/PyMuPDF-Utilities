051102951f1bc1faecff29f72b0dc32f4ffc624c
Jorj X. McKie <jorj.x.mckie@outlook.de>
2017-10-04 07:36:03 -0400

    Update gluepix.py

--- gluepix.py
+++ gluepix.py
@@ -4,19 +4,13 @@ import fitz
 PyMuPDF utility
 ----------------
 For a given entry in a page's getImagleList() list, function "recoverpix"
-returns the PNG image. This can then e.g. be saved to disk.
+returns the modified pixmap. This can then e.g. be saved to disk.
 The item's first two entries are PDF xref numbers. The first one is the image in
 question, the second one may be 0 or the object id of a soft-image mask. In this
 case, we assume it being a sequence of alpha bytes belonging to our image.
-We then create a new Pixmap giving it these alpha values, and return the PNG
-image of it.
-This an intermediate solution while PyMuPDF is developing a better one.
-So currently, we only support RGB images. A final PyMuPDF solution should look
-like this:
-(1) A new Pixmap creation method, which takes a zero-alpha input and
-    creates a copy of it with an alpha channel (filled with 0xff).
-(2) A new Pixmap method allowing to fill the alpha channel with the contents
-    of a given sequence of bytes.
+We then create a new Pixmap giving it these alpha values, and return it.
+The caller of function recoverpix may need to take special care of CMYK pixmaps
+(which cannot be saved using writePNG directly).
 """
 
 def recoverpix(doc, item):
@@ -35,4 +29,9 @@ doc = fitz.open("test.pdf")
 il = doc.getPageImageList(0)  # just process page 0
 for img in il:
     pix = recoverpix(doc, img)
+    # we may need to adjust something for CMYK pixmaps here:
+    # if pix.colorspace.n > 3:  # this is CMYK
+    #     pixc = fitz.Pixmap(fitz.csRGB, pix, 1) # create converted copy
+    #     pix = None
+    #     pix = pixc
     pix.writePNG(img[7] + ".png") # save pixmap
