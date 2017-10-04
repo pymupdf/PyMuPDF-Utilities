a4a48ec1575f04ff16f654f7e01e5f72e5c54448
Jorj X. McKie <jorj.x.mckie@outlook.de>
2017-10-04 08:29:48 -0400

    Update gluepix.py

--- gluepix.py
+++ gluepix.py
@@ -9,7 +9,7 @@ The item's first two entries are PDF xref numbers. The first one is the image in
 question, the second one may be 0 or the object id of a soft-image mask. In this
 case, we assume it being a sequence of alpha bytes belonging to our image.
 We then create a new Pixmap giving it these alpha values, and return it.
-The caller of function recoverpix may need to take special care of CMYK pixmaps
+The caller of function recoverpix should take special care of CMYK pixmaps
 (which cannot be saved using writePNG directly).
 """
 
@@ -20,18 +20,25 @@ def recoverpix(doc, item):
     if s == 0:
         return pix1 # no special handling
     pix2 = fitz.Pixmap(doc, s)  # create pixmap of /SMask entry
-    pix = fitz.Pixmap(pix1)  # copy of pix1 plus alpha channel
+    # check that we are safe
+    if not (pix1.irect == pix2.irect and \
+            pix1.alpha == pix2.alpha == 0 and \
+            pix2.n == 1):
+        pix2 = None
+        return pix1
+    pix = fitz.Pixmap(pix1)  # copy of pix1, alpha channel added
     pix.setAlpha(pix2.samples)  # treat pix2.samples as alpha value
     pix1 = pix2 = None  # free temp pixmaps
     return pix
 
 doc = fitz.open("test.pdf")
 il = doc.getPageImageList(0)  # just process page 0
+# put following in a page loop if total doc needs be scanned
 for img in il:
     pix = recoverpix(doc, img)
     # we may need to adjust something for CMYK pixmaps here:
-    # if pix.colorspace.n > 3:  # this is CMYK
-    #     pixc = fitz.Pixmap(fitz.csRGB, pix, 1) # create converted copy
-    #     pix = None
-    #     pix = pixc
-    pix.writePNG(img[7] + ".png") # save pixmap
+    if pix.colorspace.n > 3:  # this is CMYK
+        pixc = fitz.Pixmap(fitz.csRGB, pix, 1) # create converted copy
+        pix = None
+        pix = pixc
+    pix.writePNG("img-%i-%s.png" % (page.number, img[7])) # save pixmap
