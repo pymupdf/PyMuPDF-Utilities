a0b3c3d639548dbeaa4a13fa911a949837e88c78
Jorj X. McKie <jorj.x.mckie@outlook.de>
2017-10-04 07:26:45 -0400

    Update gluepix.py

--- gluepix.py
+++ gluepix.py
@@ -20,34 +20,19 @@ like this:
 """
 
 def recoverpix(doc, item):
-    x = item[0]
-    s = item[1]
+    x = item[0]  # xref of PDF image
+    s = item[1]  # xref of its /SMask
     pix1 = fitz.Pixmap(doc, x)
     if s == 0:
-        return pix1.getPNGData()
-    pix2 = fitz.Pixmap(doc, s)
-    assert pix1.n == 3, "unexpected colorspace " + pix1.colorspace.name
-    assert pix2.n == 1, "unexpected colorspace " + pix2.colorspace.name
-    balen = len(pix1.samples) + len(pix2.samples)
-    ba = bytearray(b"?" * balen)       # allocate bytearray (performance)
-    i = j = k = 0
-    while i < balen:
-        ba[i+0] = pix1.samples[j+0]    # copy over pixel data
-        ba[i+1] = pix1.samples[j+1]    # copy over pixel data
-        ba[i+2] = pix1.samples[j+2]    # copy over pixel data
-        ba[i+3] = pix2.samples[k]      # mask samples are our alpha
-        i += 4
-        j += 3
-        k += 1
-    pix = fitz.Pixmap(pix1.colorspace, pix1.width, pix1.height, ba, 1)
-    pix1 = None
-    pix2 = None
-    return pix.getPNGData()
+        return pix1 # no special handling
+    pix2 = fitz.Pixmap(doc, s)  # create pixmap of /SMask entry
+    pix = fitz.Pixmap(pix1)  # copy of pix1 plus alpha channel
+    pix.setAlpha(pix2.samples)  # treat pix2.samples as alpha value
+    pix1 = pix2 = None  # free temp pixmaps
+    return pix
 
 doc = fitz.open("test.pdf")
-il = doc.getPageImageList(0)
+il = doc.getPageImageList(0)  # just process page 0
 for img in il:
-    data = recoverpix(doc, img)
-    f = open(img[7]+".png", "wb")
-    f.write(data)
-    f.close()
+    pix = recoverpix(doc, img)
+    pix.writePNG(img[7] + ".png") # save pixmap
