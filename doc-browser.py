a134910425328e2db02f83cfbed96f5222342c17
Jorj X. McKie <jorj.x.mckie@outlook.de>
2018-08-19 09:12:19 -0400

    Tkinter script: improve inline docu

--- doc-browser.py
+++ doc-browser.py
@@ -6,31 +6,34 @@ doc = fitz.open(fname)
 title = "PyMuPDF display of '%s' (%i pages)" % (fname, len(doc))
 
 def get_page(pno, zoom = 0):
-    page = doc[pno]
-    r = page.rect
-    mp = r.tl + (r.br - r.tl) * 0.5
-    mt = r.tl + (r.tr - r.tl) * 0.5
-    ml = r.tl + (r.bl - r.tl) * 0.5
-    mr = r.tr + (r.br - r.tr) * 0.5
-    mb = r.bl + (r.br - r.bl) * 0.5
-    mat = fitz.Matrix(2, 2)
-    if zoom == 1:
+    """Return a PNG image for a document page number. If zoom is other than 0, one of the 4 page quadrants are zoomed-in instead and the corresponding clip returned.
+
+    """
+    page = doc[pno]                    # get document page
+    r = page.rect                      # page rectangle
+    mp = r.tl + (r.br - r.tl) * 0.5    # rect middle point
+    mt = r.tl + (r.tr - r.tl) * 0.5    # middle of top edge
+    ml = r.tl + (r.bl - r.tl) * 0.5    # middle of left edge
+    mr = r.tr + (r.br - r.tr) * 0.5    # middle of right egde
+    mb = r.bl + (r.br - r.bl) * 0.5    # middle of bottom edge
+    mat = fitz.Matrix(2, 2)            # zoom matrix
+    if zoom == 1:                      # top-left quadrant
         clip = fitz.Rect(r.tl, mp)
-    elif zoom == 4:
+    elif zoom == 4:                    # bot-right quadrant
         clip = fitz.Rect(mp, r.br)
-    elif zoom == 2:
+    elif zoom == 2:                    # top-right
         clip = fitz.Rect(mt, mr)
-    elif zoom == 3:
+    elif zoom == 3:                    # bot-left
         clip = fitz.Rect(ml, mb)
-    if zoom == 0:
+    if zoom == 0:                      # total page
         pix = page.getPixmap(alpha = False)
     else:
         pix = page.getPixmap(alpha = False, matrix = mat, clip = clip)
-    return pix.getPNGData()
+    return pix.getPNGData()            # return the PNG image
 
 form = sg.FlexForm(title)
 
-data = get_page(0)
+data = get_page(0)                     # show page 1 for start
 image_elem = sg.Image(data=data)
 layout = [  [image_elem],
             [sg.ReadFormButton('Next'),
@@ -46,7 +49,8 @@ layout = [  [image_elem],
 form.Layout(layout)
 
 i = 0
-oldzoom = 0
+oldzoom = 0                            # used for zoom on/off
+# the zoom buttons work in on/off mode.
 while True:
     button,value = form.Read()
     zoom = 0
