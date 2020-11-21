88f347973ddf4d70c242878689547594b558ca10
Jorj X. McKie <jorj.x.mckie@outlook.de>
2020-11-21 08:15:55 -0400

    Update extract-imga.py

--- examples/extract-imga.py
+++ examples/extract-imga.py
@@ -12,18 +12,31 @@ from PIL import Image
 """
 PyMuPDF utility
 ----------------
-For a given entry in a page's getImagleList() list, function "recoverpix"
-returns either the raw image data, or a modified pixmap if an /SMask entry
-exists.
-The item's first two entries are PDF xref numbers. The first one is the image in
-question, the second one may be 0 or the object id of a soft-image mask. In this
-case, we assume it being a sequence of alpha bytes belonging to our image.
-We then create a new Pixmap giving it these alpha values, and return it.
-If the result pixmap is CMYK, it will be converted to RGB first.
+For a given entry in a page's getImageList() list, function "recoverpix"
+returns a dictionary like the one produced by "Document.extractImage".
+It preprocesses the following special cases:
+* The PDF image has an /SMask (soft mask) entry. We use Pillow for recovering
+  the original image with an alpha channel in RGBA format.
+* The PDF image has a /ColorSpace definition. We then convert the image to
+  an RGB colorspace.
+
+The main script part implements the following features:
+- prevent multiple extractions of same image
+- prevent extraction of "unimportant" images, like "too small", "unicolor",
+  etc. This can be controlled by parameters.
+
+Apart from above special cases, the script aims to extract images with
+their original file extensions. The produced filename is "img<xref>.<ext>",
+with xref being the PDF cross reference number of the image.
 
 Dependencies
 ------------
 PyMuPDF v1.13.17+, Pillow
+
+Changes
+-------
+* 2020-10-04: for images with an /SMask, we use Pillow to recover original
+* 2020-11-21: convert cases with special /ColorSpace definitions to RGB PNG
 
 """
 print(fitz.__doc__)
@@ -32,30 +45,50 @@ if not tuple(map(int, fitz.version[0].split("."))) >= (1, 13, 17):
     raise SystemExit("require PyMuPDF v1.13.17+")
 
 dimlimit = 0  # 100  # each image side must be greater than this
-relsize = 0  # 0.05  # image : pixmap size ratio must be larger than this (5%)
+relsize = 0  # 0.05  # image : image size ratio must be larger than this (5%)
 abssize = 0  # 2048  # absolute image size limit 2 KB: ignore if smaller
 imgdir = "images"  # found images are stored in this subfolder
 
-if not os.path.exists(imgdir):
+if not os.path.exists(imgdir):  # make subfolder if necessary
     os.mkdir(imgdir)
 
 
 def recoverpix(doc, item):
-    x = item[0]  # xref of PDF image
-    s = item[1]  # xref of its /SMask
-    if s == 0:  # no smask: use direct image output
-        return doc.extractImage(x)
+    xref = item[0]  # xref of PDF image
+    smask = item[1]  # xref of its /SMask
 
-    # we need to reconstruct an alpha channel with the smask
-    fpx = io.BytesIO(doc.extractImage(x)["image"])
-    fps = io.BytesIO(doc.extractImage(s)["image"])
-    img0 = Image.open(fpx)
-    mask = Image.open(fps)
-    img = Image.new("RGBA", img0.size)
-    img.paste(img0, None, mask)
-    bf = io.BytesIO()
-    img.save(bf, "png")
-    return {"ext": "png", "colorspace": 3, "image": bf.getvalue()}
+    # special case: /SMask exists
+    # use Pillow to recover original image
+    if smask > 0:
+        fpx = io.BytesIO(  # BytesIO object from image binary
+            doc.extractImage(xref)["image"],
+        )
+        fps = io.BytesIO(  # BytesIO object from smask binary
+            doc.extractImage(smask)["image"],
+        )
+        img0 = Image.open(fpx)  # Pillow Image
+        mask = Image.open(fps)  # Pillow Image
+        img = Image.new("RGBA", img0.size)  # prepare result Image
+        img.paste(img0, None, mask)  # fill in base image and mask
+        bf = io.BytesIO()
+        img.save(bf, "png")  # save to BytesIO
+        return {  # create dictionary expected by caller
+            "ext": "png",
+            "colorspace": 3,
+            "image": bf.getvalue(),
+        }
+
+    # special case: /ColorSpace definition exists
+    # to be sure, we convert these cases to RGB PNG images
+    if "/ColorSpace" in doc.xrefObject(xref, compressed=True):
+        pix1 = fitz.Pixmap(doc, xref)
+        pix2 = fitz.Pixmap(fitz.csRGB, pix1)
+        return {  # create dictionary expected by caller
+            "ext": "png",
+            "colorspace": 3,
+            "image": pix2.getImageData("png"),
+        }
+    return doc.extractImage(xref)
 
 
 fname = sys.argv[1] if len(sys.argv) == 2 else None
@@ -67,7 +100,7 @@ if not fname:
 t0 = time.time()
 doc = fitz.open(fname)
 
-page_count = len(doc)  # number of pages
+page_count = doc.pageCount  # number of pages
 
 xreflist = []
 imglist = []
@@ -89,16 +122,16 @@ for pno in range(page_count):
         height = img[3]
         if min(width, height) <= dimlimit:
             continue
-        pix = recoverpix(doc, img)
-        n = pix["colorspace"]
-        imgdata = pix["image"]
+        image = recoverpix(doc, img)
+        n = image["colorspace"]
+        imgdata = image["image"]
 
         if len(imgdata) <= abssize:
             continue
         if len(imgdata) / (width * height * n) <= relsize:
             continue
 
-        imgfile = os.path.join(imgdir, "img-%i.%s" % (xref, pix["ext"]))
+        imgfile = os.path.join(imgdir, "img%05i.%s" % (xref, image["ext"]))
         fout = open(imgfile, "wb")
         fout.write(imgdata)
         fout.close()
