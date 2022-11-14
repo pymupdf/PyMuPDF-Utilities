7a3e6b5a0fa47715a6f6f609273a6ac828f971c1
Jorj X. McKie <jorj.x.mckie@outlook.de>
2022-11-14 10:54:40 -0400

    Make image replacement function importable
    Convert replacing images to an importable function.
    Can thus be reused to (virtually) remove an image.
    We may want to consider to make this a standard function in PyMuPDF.

--- image-replacement/remover.py
+++ image-replacement/remover.py
@@ -1,14 +1,15 @@
 import fitz
+from replacer import img_replace
 
 if tuple(map(int, fitz.VersionBind.split("."))) < (1, 19, 5):
     raise ValueError("Need v1.19.5+")
 
 # This script variant does a pseudo-removal:
-# replace image by a fully transparent pixmap
+# replace image by a small fully transparent pixmap
 doc = fitz.open("original.pdf")
 
 page = doc[0]
-page.clean_contents()  # unify page's /Contents into one
+
 images = page.get_images()  # we only are interested in first image here
 item = images[0]
 old_xref = item[0]  # old image xref
@@ -16,17 +17,6 @@ old_xref = item[0]  # old image xref
 # make a small 100% transparent pixmap (of just any dimension)
 pix = fitz.Pixmap(fitz.csGRAY, (0, 0, 1, 1), 1)
 pix.clear_with()  # clear all samples bytes to 0x00
+img_replace(page, old_xref, pixmap=pix)
 
-# insert new image just anywhere
-new_xref = page.insert_image(page.rect, pixmap=pix)
-
-# copy over definition and stream of new image
-doc.xref_copy(new_xref, old_xref)
-
-# there now is a second /Contents object, showing new image
-cont_xrefs = page.get_contents()
-
-# make sure that new /Contents is forgotten again
-page.set_contents(cont_xrefs[0])
-page.clean_contents()
 doc.ez_save("no-image.pdf", garbage=4)
--- image-replacement/replacer.py
+++ image-replacement/replacer.py
@@ -3,24 +3,37 @@ import fitz
 if tuple(map(int, fitz.VersionBind.split("."))) < (1, 19, 5):
     raise ValueError("Need v1.19.5+")
 
-doc = fitz.open("original.pdf")
-img_file = "nur-ruhig.jpg"  # the new image
-page = doc[0]
-page.clean_contents()  # unify page's /Contents into one
-images = page.get_images()  # we only are interested in first image here
-item = images[0]
-old_xref = item[0]  # old image xref
 
-# insert new image just anywhere
-new_xref = page.insert_image(page.rect, filename=img_file)
+def img_replace(page, xref, filename=None, stream=None, pixmap=None):
+    """Replace image identified by xref.
 
-# copy over definition and stream of new image
-doc.xref_copy(new_xref, old_xref)
+    Args:
+        page: a fitz.Page object
+        xref: cross reference number of image to replace
+        filename, stream, pixmap: must be given as for
+        page.insert_image().
 
-# there now is a second /Contents object, showing new image
-cont_xrefs = page.get_contents()
+    """
+    if bool(filename) + bool(stream) + bool(pixmap) != 1:
+        raise ValueError("Exactly one of filename/stream/pixmap must be given")
+    doc = page.parent  # the owning document
+    # insert new image anywhere in page
+    new_xref = page.insert_image(
+        page.rect, filename=filename, stream=stream, pixmap=pixmap
+    )
+    doc.xref_copy(new_xref, xref)  # copy over new to old
+    last_contents_xref = page.get_contents()[-1]
+    # new image insertion has created a new /Contents source,
+    # which we will set to spaces now
+    doc.update_stream(last_contents_xref, b" ")
 
-# make sure that new /Contents is forgotten
-page.set_contents(cont_xrefs[0])
-page.clean_contents()
-doc.ez_save("new-image.pdf", garbage=4, pretty=True)
+
+if __name__ == "__main__":
+    doc = fitz.open("original.pdf")
+    img_file = "nur-ruhig.jpg"  # the new image
+    page = doc[0]
+    images = page.get_images()  # we only are interested in first image here
+    item = images[0]
+    old_xref = item[0]  # old image xref
+    img_replace(page, old_xref, filename=img_file)
+    doc.ez_save("new-image.pdf", garbage=4, pretty=True)
