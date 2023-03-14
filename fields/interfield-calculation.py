ed32548659716a2a4f911d7b1859429b90f3387a
Jorj X. McKie <jorj.x.mckie@outlook.de>
2023-03-14 05:53:51 -0400

    add more comments to new scripts

--- fields/interfield-calculation.py
+++ fields/interfield-calculation.py
@@ -1,13 +1,23 @@
+"""
+Demo script for PDF widgets.
+
+Make a PDF with three pages. On each page, two fields are added and the result
+is stored in a third field on that page.
+
+Choosing three pages doing essentially the same thing, shall demonstrate,
+that field names across the whole PDF must be uniquely named.
+"""
 import fitz
 
 r1 = fitz.Rect(100, 100, 300, 120)
 r2 = fitz.Rect(100, 130, 300, 150)
 r3 = fitz.Rect(100, 180, 300, 200)
 
-doc = fitz.open()
-for i in range(3):
-    page = doc.new_page()
-    print(f"page {page.number}")
+doc = fitz.open()  # make a new, empty PDF
+for i in range(3):  # make three pages in it
+    # in essence we are causing the computation NUM1 + NUM2 = RESULT
+    page = doc.new_page()  # make the page
+
     w = fitz.Widget()
     w.field_name = f"NUM1{page.number}"
     w.rect = r1
@@ -24,14 +34,12 @@ for i in range(3):
     w.field_flags = 2
     page.add_widget(w)
 
-    w = fitz.Widget()
+    w = fitz.Widget()  # the result field
     w.field_name = f"RESULT{page.number}"
     w.rect = r3
     w.field_type = fitz.PDF_WIDGET_TYPE_TEXT
     w.field_value = "Resultat?"
     w.script_calc = f'AFSimple_Calculate("SUM", new Array("NUM1{page.number}", "NUM2{page.number}"));'
     page.add_widget(w)
-
-    print("AcroForm/CO =", doc.xref_get_key(doc.pdf_catalog(), "AcroForm/CO"))
 
 doc.save(__file__.replace(".py", ".pdf"), pretty=True)
--- fields/switch-text-on-off.py
+++ fields/switch-text-on-off.py
@@ -9,6 +9,7 @@ import fitz
 doc = fitz.open()
 page = doc.new_page()
 
+# This JavaScript will be executed if the checkbox value changes
 text_js = """if (this.getField("my-checkbox").value == "Yes")
     this.getField("my-text").display = display.visible;
 else
@@ -19,7 +20,7 @@ text.rect = fitz.Rect(100, 150, 300, 170)
 text.field_type = fitz.PDF_WIDGET_TYPE_TEXT
 text.field_name = "my-text"
 text.field_value = "Will be shown if checkbox is checked."
-text.script_calc = text_js
+text.script_calc = text_js  # use this property for inter-field actions
 page.add_widget(text)
 
 cbox = fitz.Widget()
@@ -27,7 +28,6 @@ cbox.rect = fitz.Rect(100, 100, 120, 120)
 cbox.field_type = fitz.PDF_WIDGET_TYPE_CHECKBOX
 cbox.field_name = "my-checkbox"
 cbox.field_value = True
-
 page.add_widget(cbox)
 
 doc.save(__file__.replace(".py", ".pdf"), deflate=False, pretty=True)
