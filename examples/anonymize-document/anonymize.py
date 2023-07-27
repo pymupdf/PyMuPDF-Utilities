8bc9d232f1bcf542b2cf5d1497a556984aaa2781
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-07-27 08:59:35 +0200

    anonymize: drop Python 2 code and fix for Python 3 (#109)

--- examples/anonymize-document/anonymize.py
+++ examples/anonymize-document/anonymize.py
@@ -15,8 +15,8 @@ will also be cleared with "none" values. Any XML-based metadata will also be
 deleted.
 """
 
-from __future__ import print_function
-import fitz, sys
+import sys
+import fitz
 
 
 def remove_txt(cont):
@@ -24,30 +24,23 @@ def remove_txt(cont):
     Remove everything enclosed in a pair of "BT" / "ET" strings, including both.
     Assuming "cont" is the string of a PDF "/Contents" stream, this will make
     all text of the owning page disappear (permanent delete).
-    "cont" is a bytes object in Python 3, so we will turn it into a string
-    first in that case.
     """
-    if type(cont) is bytes and str is not bytes:
-        cont1 = (cont.decode()).replace("\n", " ")
-    else:
-        cont1 = cont.replace("\n", " ")
-    ct = cont1.split(" ")
+    cont1 = cont.replace(b"\n", b" ")
+    ct = cont1.split(b" ")
     nct = []
     intext = False
     for word in ct:
-        if word == "ET":
+        if word == b"ET":
             intext = False
             continue
-        if word == "BT":
+        if word == b"BT":
             intext = True
             continue
         if intext:
             continue
         nct.append(word)
 
-    ncont = " ".join(nct)
-    if str is not bytes:  # caller expects bytes if Python 3
-        ncont = bytes(ncont, "utf-8")
+    ncont = b" ".join(nct)
     return ncont
 
 
