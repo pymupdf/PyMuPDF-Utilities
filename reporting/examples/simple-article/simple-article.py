2c6dd1fd97b8ecb115d0455874271e655a07849c
greendreamer <chrispalman@skiff.com>
2023-12-04 08:35:46 -0500

    added options and size

--- reporting/examples/simple-article/simple-article.py
+++ reporting/examples/simple-article/simple-article.py
@@ -1,11 +1,11 @@
 import pathlib
 import fitz
-from Reports import Report, Block
+from Reports import Report, Block, Options
 
 report = Report(mediabox=fitz.paper_rect("a4-l"))
 
 HTML = pathlib.Path("springer.html").read_bytes().decode()
 textblock = Block(html=HTML, report=report)
 
-report.sections = [[textblock, {"cols": 2, "format": "a4-l", "newpage": True}]]
+report.sections = [[textblock, Options(cols=2, format=fitz.Rect(0, 0, 400, 600), newpage=True)]]
 report.run("output.pdf")
